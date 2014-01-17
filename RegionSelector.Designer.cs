441add2c05a2454d63ca2beb795f8e2e1a39b246
factormystic <factormystic@gmail.com>
2014-01-16 23:21:51 -0500

    Fix three of four links not working - small positioning tweak, much like rearranging the deck chairs on the titanic

--- RegionSelector.Designer.cs
+++ RegionSelector.Designer.cs
@@ -41,18 +41,20 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BackColor = System.Drawing.Color.Fuchsia;
-            this.panel1.Location = new System.Drawing.Point(20, 20);
+            this.panel1.Location = new System.Drawing.Point(25, 25);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(290, 290);
+            this.panel1.Size = new System.Drawing.Size(362, 362);
             this.panel1.TabIndex = 0;
             // 
             // llTakeScreenshotTopLeft
             // 
             this.llTakeScreenshotTopLeft.AutoSize = true;
             this.llTakeScreenshotTopLeft.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(69)))), ((int)(((byte)(173)))));
-            this.llTakeScreenshotTopLeft.Location = new System.Drawing.Point(17, 2);
+            this.llTakeScreenshotTopLeft.Location = new System.Drawing.Point(21, 2);
+            this.llTakeScreenshotTopLeft.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.llTakeScreenshotTopLeft.Name = "llTakeScreenshotTopLeft";
-            this.llTakeScreenshotTopLeft.Size = new System.Drawing.Size(93, 15);
+            this.llTakeScreenshotTopLeft.Size = new System.Drawing.Size(116, 20);
             this.llTakeScreenshotTopLeft.TabIndex = 1;
             this.llTakeScreenshotTopLeft.TabStop = true;
             this.llTakeScreenshotTopLeft.Text = "Take Screenshot";
@@ -63,46 +65,52 @@
             this.llTakeScreenshotBottomRight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.llTakeScreenshotBottomRight.AutoSize = true;
             this.llTakeScreenshotBottomRight.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(69)))), ((int)(((byte)(173)))));
-            this.llTakeScreenshotBottomRight.Location = new System.Drawing.Point(217, 313);
+            this.llTakeScreenshotBottomRight.Location = new System.Drawing.Point(271, 389);
+            this.llTakeScreenshotBottomRight.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.llTakeScreenshotBottomRight.Name = "llTakeScreenshotBottomRight";
-            this.llTakeScreenshotBottomRight.Size = new System.Drawing.Size(93, 15);
+            this.llTakeScreenshotBottomRight.Size = new System.Drawing.Size(116, 20);
             this.llTakeScreenshotBottomRight.TabIndex = 2;
             this.llTakeScreenshotBottomRight.TabStop = true;
             this.llTakeScreenshotBottomRight.Text = "Take Screenshot";
             this.llTakeScreenshotBottomRight.Visible = false;
+            this.llTakeScreenshotBottomRight.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // llTakeScreenshotTopRight
             // 
             this.llTakeScreenshotTopRight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.llTakeScreenshotTopRight.AutoSize = true;
             this.llTakeScreenshotTopRight.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(69)))), ((int)(((byte)(173)))));
-            this.llTakeScreenshotTopRight.Location = new System.Drawing.Point(217, 2);
+            this.llTakeScreenshotTopRight.Location = new System.Drawing.Point(271, 2);
+            this.llTakeScreenshotTopRight.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.llTakeScreenshotTopRight.Name = "llTakeScreenshotTopRight";
-            this.llTakeScreenshotTopRight.Size = new System.Drawing.Size(93, 15);
+            this.llTakeScreenshotTopRight.Size = new System.Drawing.Size(116, 20);
             this.llTakeScreenshotTopRight.TabIndex = 3;
             this.llTakeScreenshotTopRight.TabStop = true;
             this.llTakeScreenshotTopRight.Text = "Take Screenshot";
             this.llTakeScreenshotTopRight.Visible = false;
+            this.llTakeScreenshotTopRight.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // llTakeScreenshotBottomLeft
             // 
             this.llTakeScreenshotBottomLeft.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.llTakeScreenshotBottomLeft.AutoSize = true;
             this.llTakeScreenshotBottomLeft.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(69)))), ((int)(((byte)(173)))));
-            this.llTakeScreenshotBottomLeft.Location = new System.Drawing.Point(17, 313);
+            this.llTakeScreenshotBottomLeft.Location = new System.Drawing.Point(21, 389);
+            this.llTakeScreenshotBottomLeft.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.llTakeScreenshotBottomLeft.Name = "llTakeScreenshotBottomLeft";
-            this.llTakeScreenshotBottomLeft.Size = new System.Drawing.Size(93, 15);
+            this.llTakeScreenshotBottomLeft.Size = new System.Drawing.Size(116, 20);
             this.llTakeScreenshotBottomLeft.TabIndex = 4;
             this.llTakeScreenshotBottomLeft.TabStop = true;
             this.llTakeScreenshotBottomLeft.Text = "Take Screenshot";
             this.llTakeScreenshotBottomLeft.Visible = false;
+            this.llTakeScreenshotBottomLeft.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // RegionSelector
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(330, 330);
+            this.ClientSize = new System.Drawing.Size(412, 412);
             this.Controls.Add(this.llTakeScreenshotBottomLeft);
             this.Controls.Add(this.llTakeScreenshotTopRight);
             this.Controls.Add(this.llTakeScreenshotBottomRight);
@@ -111,7 +119,8 @@
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.KeyPreview = true;
-            this.MinimumSize = new System.Drawing.Size(41, 41);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MinimumSize = new System.Drawing.Size(51, 51);
             this.Name = "RegionSelector";
             this.Opacity = 0.75D;
             this.ShowIcon = false;
